9081f89ea581e369e1de8812640808d99f09f1e6
Dylan Thomas <thomasdylan76@gmail.com>
2019-11-10 15:00:41 -0700

    Logic for finding friend.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,26 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function(req, res) {
+        var friend = friendsList[0].name;
+        var totalDifference = 0;
+        var difference = 1000;
+        var scores = req.body.scores;
+
+        for(var i = 0; i < friendsList.length; i++) {
+            totalDifference = 0;
+            for(var j = 0; j < scores.length; j++) {
+                totalDifference += Math.abs(parseInt(scores[j]) - parseInt(friendsList[i].scores[j]));
+            };
+            if(totalDifference < difference) {
+                friend = friendsList[i].name;
+                difference = totalDifference;
+            };
+        };
+
+        console.log(friend);
         friendsList.push(req.body);
     });
+
+    
 
 };
