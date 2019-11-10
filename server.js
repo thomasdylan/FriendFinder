1cef8caab79868e0d2cfa6a05991f57708a5fcfe
Dylan Thomas <thomasdylan76@gmail.com>
2019-11-10 16:13:48 -0700

    fixed port.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const friends = require("./app/data/friends.js");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
